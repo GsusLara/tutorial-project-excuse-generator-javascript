e0782858230bba0ce2f565d9162f99ede18fc9e1
Jesus Lara <jelara99@gmail.com>
2021-02-10 16:00:14 +0000

    trabajo procurso terminado

---
+++ script.js
@@ -0,0 +1,68 @@
+let escusa="";
+var selector=Math.floor(Math.random() * (21 - 1)) + 1;
+
+switch (selector) {
+    case 1:
+        excusa="Habia muchisima presa!!"
+        break;
+    case 2:
+        excusa="llegue a tiempo y me fui al baño!!"
+        break;
+    case 3:
+        excusa="Se fue la luz en casa"
+        break;
+    case 4:
+        excusa="No había agua en casa"
+        break;
+    case 5:
+        excusa="mi tataratatarabuela se murío"
+        break;
+    case 6:
+        excusa="estaban cerradas todas las calles"
+        break;
+    case 7:
+        excusa="Había una huelga gigante"
+        break;
+    case 8:
+        excusa="me aparecio un marciano en mi casa"
+        break;
+    case 9:
+        excusa="vi un fantasma y me desmayé del susto"
+        break;
+    case 10:
+        excusa="casi me muero"
+        break;
+    case 11:
+        excusa="aqui estaba hace rato"
+        break;
+    case 12:
+        excusa="me topé a Donal trump de camino"
+        break;
+    case 13:
+        excusa="el bus se varó"
+        break;
+    case 14:
+        excusa="se me dañó el despertador"
+        break;
+    case 15:
+        excusa="me tuve que devolver porque deje la billetera"
+        break;
+    case 16:
+        excusa="estoy mal del estomago"
+        break;
+    case 17:
+        excusa="ya ya ya estoy por llegar"
+        break;
+    case 18:
+        excusa="nunca me ha pasado esto"
+        break;
+    case 19:
+        excusa="me quisieron robar"
+        break;
+    case 20:
+        excusa="vi a superman"
+        break;
+    default:
+        break;
+}
+document.write('<h1 id="textaleatorio">'+excusa+'<h1>');
