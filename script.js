8e724819efb977064cb9e2278f044a4502aa30a4
Jesus Lara <jelara99@gmail.com>
2021-02-14 02:37:08 +0000

    correccion de codigo rigido

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let who = ['the dog','my granma','the turtle','my bird'];
 let what = ['eat','pissed','crushed','broked'];
 let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
 
-let opwho=Math.floor(Math.random() * (4 - 0)) + 0;
-let opwhat=Math.floor(Math.random() * (4 - 0)) + 0;
-let opwhen=Math.floor(Math.random() * (5 - 0)) + 0;
+let opwho=Math.floor(Math.random() * who.length);
+let opwhat=Math.floor(Math.random() * what.length);
+let opwhen=Math.floor(Math.random() * when.length);
 
 document.write('<h1 id="textaleatorio">'+who[opwho]+' '+what[opwhat]+' my homework '+when[opwhen]+'<h1>');
